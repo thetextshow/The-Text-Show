a556c580e8c78e14b191b0fde16286733ddc8543
lfahmy <admin@thetext.show>
2024-02-18 17:01:45 -0800

    refactored forms.js
    Created calendar.js to hold the calendar logic

--- forms.js
+++ forms.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const addToCalendar = require('./calendar.js');
 
 const app = express();
 app.use(express.json()); 
@@ -45,7 +46,8 @@ app.post('/', (req, res) => {
   console.log("\n\n--- Paid Keys ---");
   console.log(paidInputs);
 
-  buildText(freeInputs, 'Free');
+  const freeEvent = buildText(freeInputs, 'Free');
+  addToCalendar(freeEvent);
 
   res.sendStatus(200);
 });
@@ -61,14 +63,14 @@ function buildText(input, type) {
   const message = input['numWinners'] === 1 ?
     input['question'].concat('\n\n', "Fastest person wins $", input['prize'], ".")
     : input['question'].concat('\n\n', "Fastest ", input['numWinners'], " people win $", input['prize'], " each.");
-  console.log(message);
 
+  // end time is one hour after start time
   const end = input['date'].substr(0, 12)
     + (parseInt(input['date'][12]) + 1).toString()
     + input['date'].substr(13);
 
-  const event = {
-    'summary': type + ' Question ' + input['date'].substr(0, 10),
+  return {
+    'summary': type + ' Question ' + input['date'].substr(5, 5),
     'description': message,
     'start': {
       'dateTime': input['date']
@@ -77,116 +79,6 @@ function buildText(input, type) {
       'dateTime': end
     }
   };
-
-  console.log(event);
-
-  authorize().then((auth) => {
-    createEvent(auth, event);
-  }).catch(console.error);
 }
 
-
-
-
-
-
-const fs = require('fs').promises;
-const path = require('path');
-//const process = require('process');
-const {authenticate} = require('@google-cloud/local-auth');
-const {google} = require('googleapis');
-
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
-
-/**
- * Reads previously authorized credentials from the save file.
- *
- * @return {Promise<OAuth2Client|null>}
- */
-async function loadSavedCredentialsIfExist() {
-  try {
-    const content = await fs.readFile(TOKEN_PATH);
-    const credentials = JSON.parse(content);
-    return google.auth.fromJSON(credentials);
-  } catch (err) {
-    return null;
-  }
-}
-
-/**
- * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
-async function saveCredentials(client) {
-  const content = await fs.readFile(CREDENTIALS_PATH);
-  const keys = JSON.parse(content);
-  const key = keys.installed || keys.web;
-  const payload = JSON.stringify({
-    type: 'authorized_user',
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token,
-  });
-  await fs.writeFile(TOKEN_PATH, payload);
-}
-
-/**
- * Load or request or authorization to call APIs.
- *
- */
-async function authorize() {
-  let client = await loadSavedCredentialsIfExist();
-  if (client) {
-    return client;
-  }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
-  });
-  if (client.credentials) {
-    await saveCredentials(client);
-  }
-  return client;
-}
-
-// Refer to the Node.js quickstart on how to setup the environment:
-// https://developers.google.com/calendar/quickstart/node
-// Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
-// stored credentials.
-
-const event = {
-  'summary': 'Google I/O 2015',
-  'description': 'A chance to hear more about Google\'s developer products.',
-  'start': {
-    'dateTime': '2024-02-14T16:00:00.000Z'
-  },
-  'end': {
-    'dateTime': '2024-02-14T17:00:00.000Z'
-  }
-};
-
-async function createEvent(auth, event) {
-  const calendar = google.calendar({version: 'v3', auth});
-  calendar.events.insert({
-    auth: auth,
-    calendarId: 'primary',
-    resource: event,
-  }, function(err, event) {
-    if (err) {
-      console.log('There was an error contacting the Calendar service: ' + err);
-      return;
-    }
-    console.log('Event created: %s', event.data);
-  });
-}
-
-//authorize().then(createEvent).catch(console.error);
 
