6581f835faa48cb0eae67381f00c53dfbcea7c2f
lfahmy <admin@thetext.show>
2024-02-19 18:55:17 -0800

    cloud tasks rudimentary
    holy shit this took forever. will for sure break once i containerize and deploy to cloud run

--- forms/tasks.js
+++ forms/tasks.js
@@ -4,13 +4,14 @@ const {CloudTasksClient} = require('@google-cloud/tasks');
 // Instantiates a client.
 const client = new CloudTasksClient();
 
-async function createHttpTaskWithToken() {
+async function createHttpTask() {
+  // TODO(developer): Uncomment these lines and replace with your values.
   const project = 'the-text-show';
   const queue = 'tts-send-trigger';
   const location = 'us-central1';
   const url = 'https://2bbd-2600-1700-cbd0-10a0-91b9-b5e9-f1e8-8b0b.ngrok-free.app';
-  const serviceAccountEmail = 'tasks-service-account@the-text-show.iam.gserviceaccount.com';
   const payload = 'Hello, World!';
+  const inSeconds = 180;
 
   // Construct the fully qualified queue name.
   const parent = client.queuePath(project, location, queue);
@@ -22,9 +23,6 @@ async function createHttpTaskWithToken() {
       },
       httpMethod: 'POST',
       url,
-      oidcToken: {
-        serviceAccountEmail,
-      },
     },
   };
 
@@ -32,12 +30,18 @@ async function createHttpTaskWithToken() {
     task.httpRequest.body = Buffer.from(payload).toString('base64');
   }
 
+  if (inSeconds) {
+    // The time when the task is scheduled to be attempted.
+    task.scheduleTime = {
+      seconds: parseInt(inSeconds) + Date.now() / 1000,
+    };
+  }
+
+  // Send create task request.
   console.log('Sending task:');
   console.log(task);
-  // Send create task request.
   const request = {parent: parent, task: task};
   const [response] = await client.createTask(request);
-  const name = response.name;
-  console.log(`Created task ${name}`);
+  console.log(`Created task ${response.name}`);
 }
-createHttpTaskWithToken();
+createHttpTask();
