989768c79b45ce4aae5effdbe412d332fcddc125
thetextshow <158445342+thetextshow@users.noreply.github.com>
2024-04-12 18:42:07 -0700

    Update MSG.js

--- messaging/MSG.js
+++ messaging/MSG.js
@@ -1,18 +1,19 @@
 function format(message, ...values) {
   values.forEach((value, index) => {
-		  message = message.replace(`%${index}`, value);
+		  message = message.replace(`%${index}%`, value);
 		});
 		return message;
 }
 
 const MSG = Object.freeze({
 	JOIN: "Send PLAY to opt into The Text Show!",
-	NOT_KEY: "%0 is not a keyword. Type HELP to see a list of keywords.",
+	NOT_KEY: "%0% is not a keyword. Type HELP to see a list of keywords.",
 	PLAY: "You typed PLAY",
 	WELCOME: "Welcome!",
 	HELP: "You typed HELP",
 	STOP: "You have been unsubscribed from The Text Show. Send PLAY to resubscribe.",
 	ALL_CORRECT: "Correct!\n\n" + "You got everything correct! We'll let you know if you won soon.",
+	CORRECT: "Correct! Next Question:\n\n%0%\n\nOnly your FIRST answer will be considered.",
 	WRONG: "WRONG !!! u LOSE",
 	WON: "You actually won."
 });
